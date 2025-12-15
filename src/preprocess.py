31bccabb0c4fce99018be3a6d81d68278c7ecdb0
anjaliabhilashcv <anjali.abhilashcv@gmail.com>
2025-12-16 01:37:29 +0530

    Update preprocess.py

--- src/preprocess.py
+++ src/preprocess.py
@@ -17,3 +17,30 @@ def normalize(df, cols):
 def detect_duplicates(df):
     dup=df.duplicated()
     return df[dup]
+
+import pickle
+from pathlib import Path
+
+ARTIFACTS_DIR = Path("artifacts")
+SCALER_PATH = ARTIFACTS_DIR / "scaler.pkl"
+
+def normalize_features(df, feature_cols, target_col):
+
+    #ensuring that target is not normalised
+    feature_cols = [col for col in feature_cols if col != target_col]
+
+    #applying normalisation
+    df, scaler = normalize(df, feature_cols)
+
+    #creating artifacts directory if not exists
+    ARTIFACTS_DIR.mkdir(parents=True, exist_ok=True)
+
+    with open(SCALER_PATH, "wb") as f:
+        pickle.dump(scaler, f)
+
+    #printing transformed values
+    print("✅ Feature range after MinMax scaling:")
+    print(f"Min: {df[feature_cols].min().min():.4f}")
+    print(f"Max: {df[feature_cols].max().max():.4f}")
+
+    return df
